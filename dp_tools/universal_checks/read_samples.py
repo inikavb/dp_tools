f46bf157dd4947aedafba1311637088aa6a21adc
inikavb <107143237+inikavb@users.noreply.github.com>
2022-10-31 12:52:15 -0700

    Delete read_samples.py

--- dp_tools/universal_checks/read_samples.py
+++
@@ -1,7 +0,0 @@
-def read_samples(file_path):
-    """ reading unique sample names into list """
-
-    with open(file_path) as f:
-        sample_names = f.read().splitlines()
-
-    return(sample_names)
