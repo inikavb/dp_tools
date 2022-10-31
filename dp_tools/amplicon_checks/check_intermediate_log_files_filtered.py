ddf118f946d6c56e201c16699b072466f04e34bb
inikavb <107143237+inikavb@users.noreply.github.com>
2022-10-31 12:43:49 -0700

    Create check_intermediate_log_files_filtered.py

---
+++ dp_tools/amplicon_checks/check_intermediate_log_files_filtered.py
@@ -0,0 +1,11 @@
+def check_intermediate_log_files_filtered(entry : path):
+    
+    output_files_present = [f for f in os.listdir(filtered_reads_dir) if os.path.isfile(os.path.join(filtered_reads_dir, f))]
+    if not any(output_file.endswith(entry) for output_file in output_files_present):
+        code = FlagCode.HALT
+        message = "An output file named or ending with '" + str(entry) + "' was expected but not found in " + str(filtered_reads_dir) + "."
+    else:
+        code = FlagCode.GREEN
+        message = "The expected output file named or ending with " + str(entry) + " was found."
+
+    return {"code": code, "message": message}
