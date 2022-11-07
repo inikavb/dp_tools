999d172442e5489748f340a41bb7b1e69f5fb51e
inikavb <107143237+inikavb@users.noreply.github.com>
2022-11-06 20:27:18 -0800

    Update check_expected_directories.py

--- dp_tools/universal_checks/check_expected_directories.py
+++ dp_tools/universal_checks/check_expected_directories.py
@@ -1,19 +1,20 @@
 def check_expected_directories(directory : path):
-    """ checks expected directories exist """
-
+    """Checks expected directories exist
+    This is achieved by checking if the 'os.path.isdire" exists given a directory.
+    An optional name_reformat_function can be supplied to address sample name changes that occur in the multiqc report.
+    An example being the renaming of Sample '-' characters to '_' for certain RSeQC modules.
+    
+    :param directory: Directory name
+    :type directory: str
+    :param path: MultiQC report directory
+    :type path: Path
+    :return: Flag Entry denoting successful or failing results. Includes description of directory names and any missing directories
+    :rtype: FlagEntry
+    """
     if not os.path.isdir(directory):
         code = FlagCode.HALT
         message = "The directory '" + str(directory) + "' was expected but not found."
-        #report_failure("The directory '" + str(directory) + "' was expected but not found.")
     else: 
         code = FlagCode.GREEN
         message = "The directory " + str(directory)+ " exists."
     return {"code": code, "message": message}
-
-def read_samples(file_path):
-    """ reading unique sample names into list """
-
-    with open(file_path) as f:
-        sample_names = f.read().splitlines()
-
-    return(sample_names)
