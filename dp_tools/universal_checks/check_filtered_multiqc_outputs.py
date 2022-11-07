2311c11158de177ec0c44a0aa9dab13215be3022
inikavb <107143237+inikavb@users.noreply.github.com>
2022-11-06 20:31:55 -0800

    Update check_filtered_multiqc_outputs.py

--- dp_tools/universal_checks/check_filtered_multiqc_outputs.py
+++ dp_tools/universal_checks/check_filtered_multiqc_outputs.py
@@ -1,5 +1,14 @@
 def check_filtered_multiqc_outputs(sample : path):
-    # checking filtered
+    """Checks the filtered multiqc outputs in the multiqc report.
+    This is achieved by checking the multiqc outputs in the multiqc report (Change this w more specific language).
+    
+    :param sample: Query sample names to check for presence in multiqc outputs
+    :type sample: list[str]
+    :param path: MultiQC report directory
+    :type path: Path
+    :return: Flag Entry denoting successful or failing results. Includes description of multiqc output names and any missing outputs
+    :rtype: FlagEntry
+    """
     zip_file = zipfile.ZipFile(filt_multiqc_data_path)
     df = pd.read_csv(zip_file.open("multiqc_general_stats.txt"), sep = "\t", usecols = ["Sample"])
     file_prefixes_in_multiqc = df["Sample"].tolist()
